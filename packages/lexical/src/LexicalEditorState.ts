9435de7baa6e9a37be5adc2eac4f42420d718456
Mateo Vuković <172550879+vukied@users.noreply.github.com>
2024-06-18 22:57:25 +0200

    Start working on editor state

--- packages/lexical/src/LexicalEditorState.ts
+++ packages/lexical/src/LexicalEditorState.ts
@@ -1,5 +1,13 @@
+import { NodeMap } from './LexicalNode';
+
 export function createEmptyEditorState(): EditorState {
-  return new EditorState();
+  return new EditorState(new Map([['root', {}]]));
 }
 
-export class EditorState {}
+export class EditorState {
+  _nodeMap: NodeMap;
+
+  constructor(nodeMap: NodeMap) {
+    this._nodeMap = nodeMap;
+  }
+}
